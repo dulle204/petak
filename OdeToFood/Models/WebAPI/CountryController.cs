eea000e320cdca621cd924191466d9d0caf2f198
d.ivanisevic <d.ivanisevic@DIVANISEVIC>
2016-09-23 17:14:38 +0200

    Add project files.

---
+++ OdeToFood/Models/WebAPI/CountryController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Newtonsoft.Json;
+using System.Web.Mvc;
+
+namespace OdeToFood.Models.WebAPI
+{
+    public class CountryController : ApiController
+    {
+        // GET api/country
+        [System.Web.Http.HttpGet]
+        public JsonResult Get()
+        {
+            var retVal = JsonConvert.SerializeObject(LoadCountries());
+            JsonResult json = new JsonResult()
+            {
+                Data = retVal
+            };
+            return json;
+        }
+
+        private List<string> LoadCountries()
+        {
+            List<string> list = new List<string>();
+
+            list.Add("Serbia");
+            list.Add("USA");
+            list.Add("France");
+            list.Add("Canada");
+
+            return list;
+        }
+
+        // GET api/country/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/country
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/country/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/country/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ OdeToFood/Scripts/dropDownLoader.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        url: "http://localhost:56470/api/country",
+        type: "Get",
+        success: function (data) {
+            $.each(data, function (index, item) {
+                $('#ddCountry').append($('<option></option>').val(item.Name).html(item.Name));
+            })
+        },
+        error: function (msg) { alert(msg); }
+    });
+});
